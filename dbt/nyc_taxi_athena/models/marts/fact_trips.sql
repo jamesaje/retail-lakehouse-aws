69c05de0f4666687aead49a37f742ca6642be1d4
Femi <jamesaje@yahoo.com>
2026-01-16 15:48:28 +0000

    Full en-to-end run

--- dbt/nyc_taxi_athena/models/marts/fact_trips.sql
+++ dbt/nyc_taxi_athena/models/marts/fact_trips.sql
@@ -1,22 +1,37 @@
 {{ config(
     materialized='incremental',
     incremental_strategy='insert_overwrite',
-    partitioned_by=['pickup_year', 'pickup_month']
+    partitioned_by=['pickup_year', 'pickup_month'],
+    unique_key='trip_id'
 ) }}
 
 with trips as (
 
-    select *
+    select distinct
+        vendor_id,
+        passenger_count,
+        rate_code_id,
+        pu_location_id,
+        do_location_id,
+        payment_type,
+        pickup_ts,
+        dropoff_ts,
+        trip_distance,
+        fare_amount,
+        tip_amount,
+        tolls_amount,
+        total_amount,
+        pickup_year,
+        pickup_month
     from {{ ref('stg_yellow_trips') }}
+    where pickup_ts is not null
+      and dropoff_ts is not null
+      and dropoff_ts >= pickup_ts
+      and date_diff('second', pickup_ts, dropoff_ts) between 0 and 86400
 
     {% if is_incremental() %}
-      -- On incremental runs, only process months >= the latest month already in the target table
-      where (pickup_year, pickup_month) >= (
-        select
-          max(pickup_year) as pickup_year,
-          max(pickup_month) as pickup_month
-        from {{ this }}
-      )
+      and pickup_year = {{ var('year') }}
+      and pickup_month = {{ var('month') }}
     {% endif %}
 
 ),
@@ -24,8 +39,27 @@ with trips as (
 final as (
 
     select
-        -- Natural key for now (we'll improve later with hashing when you ingest more months)
-        row_number() over (order by pickup_ts, dropoff_ts, pu_location_id, do_location_id) as trip_id,
+        -- Deterministic trip_id: stable across reruns/months
+        lower(to_hex(md5(
+          to_utf8(
+            concat_ws('|',
+              cast(pickup_ts as varchar),
+              cast(dropoff_ts as varchar),
+              cast(vendor_id as varchar),
+              cast(rate_code_id as varchar),
+              cast(payment_type as varchar),
+              cast(pu_location_id as varchar),
+              cast(do_location_id as varchar),
+              cast(coalesce(passenger_count, 0) as varchar),
+              cast(coalesce(trip_distance, 0.0) as varchar),
+              cast(coalesce(fare_amount, 0.0) as varchar),
+              cast(coalesce(tip_amount, 0.0) as varchar),
+              cast(coalesce(tolls_amount, 0.0) as varchar),
+              cast(coalesce(total_amount, 0.0) as varchar)
+            )
+          )
+        ))) as trip_id,
+
 
         date(pickup_ts) as pickup_date,
 
@@ -52,10 +86,7 @@ final as (
         pickup_month
 
     from trips
-    where pickup_ts is not null
-      and dropoff_ts is not null
-      and dropoff_ts >= pickup_ts
-      and date_diff('second', pickup_ts, dropoff_ts) between 0 and 86400
+
 )
 
 select * from final
